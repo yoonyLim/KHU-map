e7cdb0dcc29a90cac7b54a1d266449f83af18a10
Hayoon Lim <hayoon603@gmail.com>
2023-10-30 22:02:03 +0900

    index.html: margin fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
     import { onMounted, ref } from "vue";
-    import { Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, SphereGeometry, WebGLRenderer } from "three"
+    import { Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, SphereGeometry, WebGLRenderer, DirectionalLight, AxesHelper } from "three"
+    import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
     import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
 
     const canvas = ref(null)
@@ -15,10 +16,19 @@
         0.1, 
         1000
     );
-
     camera.position.z = 5;
-
+    camera.layers.enableAll();
     scene.add(camera)
+
+    const dirLight = new DirectionalLight( 0xffffff, 3 );
+    dirLight.position.set( 0, 0, 1 );
+    dirLight.layers.enableAll();
+    scene.add( dirLight );
+
+    // axes for positioning
+    const axesHelper = new AxesHelper( 5 );
+    axesHelper.layers.enableAll();
+    scene.add( axesHelper );
 
     // mesh
     const sphere = new Mesh(
@@ -38,9 +48,9 @@
 
     const sphereLabel = new CSS2DObject(sphereDiv);
     sphereLabel.position.set(1.5 * 20, 0, 0);
-    sphereLabel.center.set(0, 0);
+    sphereLabel.center.set(0, 1);
     sphere.add(sphereLabel);
-    sphereLabel.layers.set(0);
+    sphereLabel.layers.set(1);
 
     onMounted(() => {
         // set renderer to render the scene
@@ -59,6 +69,11 @@
         labelRenderer.setSize(window.innerWidth, window.innerHeight);
         labelRenderer.domElement.style.position = "absolute";
         labelRenderer.domElement.style.top = "0px";
+        document.body.appendChild(labelRenderer.domElement);
+
+        const controls = new OrbitControls( camera, labelRenderer.domElement );
+        controls.minDistance = 5;
+        controls.maxDistance = 100;
 
         labelRenderer.render(scene, camera);
     })
