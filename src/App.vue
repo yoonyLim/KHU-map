0549d692099b7b07019feefd11b3ae84bef1da82
Hayoon Lim <hayoon603@gmail.com>
2023-10-31 03:26:00 +0900

    app.vue: added animation and controls

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,86 @@
 <script setup>
-    import { onMounted, ref } from "vue";
-    import { Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, SphereGeometry, WebGLRenderer, DirectionalLight, AxesHelper } from "three"
+    import { computed, onMounted, onUnmounted, ref, watch } from "vue";
+    import { Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer, DirectionalLight, AxesHelper, BoxGeometry } from "three"
     import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
     import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
 
     const canvas = ref(null)
 
+    // declaration for universal usage
+    let renderer;
+    let camera;
+    let controls;
+
     // scene where everything takes place
-    const scene = new Scene()
+    const scene = new Scene();
+
+    let aspectRatio = computed(() => window.innerWidth / window.innerHeight);
 
     // camera to view the scene
-    const camera = new PerspectiveCamera(
+    camera = new PerspectiveCamera(
         75, 
-        window.innerWidth / window.innerHeight, 
+        aspectRatio.value, 
         0.1, 
         1000
     );
     camera.position.z = 5;
-    camera.layers.enableAll();
+    // camera.layers.enableAll();
     scene.add(camera)
 
     const dirLight = new DirectionalLight( 0xffffff, 3 );
     dirLight.position.set( 0, 0, 1 );
     dirLight.layers.enableAll();
-    scene.add( dirLight );
+    scene.add(dirLight);
 
     // axes for positioning
     const axesHelper = new AxesHelper( 5 );
     axesHelper.layers.enableAll();
-    scene.add( axesHelper );
+    scene.add(axesHelper);
 
     // mesh
-    const sphere = new Mesh(
-        new SphereGeometry(1, 20, 20),
+    const cube = new Mesh(
+        new BoxGeometry(1, 1, 1, 32, 32),
         new MeshBasicMaterial({
             color: 0x008080
         })
     );
 
-    scene.add(sphere);
+    scene.add(cube);
 
     // labels for mesh
-    const sphereDiv = document.createElement('div');
-    sphereDiv.className = "label";
-    sphereDiv.textContent = "Sphere";
-    sphereDiv.style.backgroundColor = "transparent";
+    const cubeDiv = document.createElement('div');
+    cubeDiv.className = "label";
+    cubeDiv.textContent = "Cube";
+    cubeDiv.style.backgroundColor = "transparent";
 
-    const sphereLabel = new CSS2DObject(sphereDiv);
-    sphereLabel.position.set(1.5 * 20, 0, 0);
-    sphereLabel.center.set(0, 1);
-    sphere.add(sphereLabel);
-    sphereLabel.layers.set(1);
+    const cubeLabel = new CSS2DObject(cubeDiv);
+    cubeLabel.position.set(1.5 * 10, 0, 0);
+    cubeLabel.center.set(0, 1);
+    cube.add(cubeLabel);
+    cubeLabel.layers.set(1);
 
+    const loop = () => {
+        renderer.render(scene, camera);
+        controls.update();
+
+        // rotate animation
+        cube.rotation.y += 0.01;
+        cube.rotation.x += 0.01;
+        requestAnimationFrame(loop);
+    }
+    
     onMounted(() => {
         // set renderer to render the scene
-        const renderer = new WebGLRenderer({
+        renderer = new WebGLRenderer({
             canvas: canvas.value,
             antialias: true
         });
 
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        renderer.setPixelRatio(window.devicePixelRatio);
+        // for initial size
+        updateSize();
 
-        renderer.render(scene, camera);
+        // for window to listen to resize and update the size
+        window.addEventListener("resize", updateSize);
 
         // set label renderer to render label
         const labelRenderer = new CSS2DRenderer();
@@ -71,12 +89,34 @@
         labelRenderer.domElement.style.top = "0px";
         document.body.appendChild(labelRenderer.domElement);
 
-        const controls = new OrbitControls( camera, labelRenderer.domElement );
-        controls.minDistance = 5;
-        controls.maxDistance = 100;
+        // labelRenderer가 canvas 위에 덮이기 때문에 labelRenderer를 통해서 컨트롤 가능
+        controls = new OrbitControls(camera, labelRenderer.domElement);
+        controls.enableDamping = true;
+        controls.minDistance = 3;
+        controls.maxDistance = 50;
 
         labelRenderer.render(scene, camera);
+
+        // constantly render by frame to keep ratio constant
+        loop();
     })
+
+    onUnmounted(() => {
+        window.removeEventListener("resize", updateSize);
+    })
+
+    // function to be called when window is resiszed
+    function updateSize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        console.log(window.innerWidth);
+    }
+
+    // watch for window size change and call functions
+    watch(aspectRatio, updateSize);
 </script>
 
 <template>
