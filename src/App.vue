c41916b87bf448271a963d0b15efcf904bae117a
Hayoon Lim <hayoon603@gmail.com>
2023-11-01 21:37:30 +0900

    homeview.vue: new input field over the canvas

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-    import HomeView from './views/HomeView.vue';
+    import { RouterView } from 'vue-router'
 </script>
 
 <template>
     <div>
-        <HomeView />
+        <RouterView />
     </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+     extend: {},
+   },
+   variants: {
+     extend: {},
+   },
+   plugins: [],
+ }
 
